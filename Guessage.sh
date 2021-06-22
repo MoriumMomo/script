a089b96bee06c64a6c3971991b8e78ece205be8e
MoriumMomo <61787214+MoriumMomo@users.noreply.github.com>
2021-06-22 12:01:08 +0800

    Add files via upload

---
+++ Guessage.sh
@@ -0,0 +1,31 @@
+# !/bin/sh
+#student Name: Morium Mostafa Momo
+#student ID: 10492778
+
+# welcome note for the user 
+
+echo $'\n**** WELCOME TO GUESSING GAME ****'
+echo "Please enter your guess: "
+read guess_age
+
+# generate a random number between 20 and 70
+
+(( age = RANDOM % 50 + 20 ))
+
+while [[ $guess_age -ne $age ]]  # continue until the guessed age is not equal to the age
+do
+	if [[ $guess_age -gt $age ]]  # check wheather the guessed age is greater than  the age
+	then
+		echo "The guess is too high!"
+	else 
+		echo "The guess is too low!"
+	fi
+	echo
+	echo "Guess a new age: "
+	read guess_age
+done
+
+echo "The guess is correct!"
+echo $'**** THE END ****\n'
+
+# end of the script
